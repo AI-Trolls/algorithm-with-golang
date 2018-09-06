ec4b08232ce83843bc56fe1127ccc181100f24a9
songjein <jeinsong100@gmail.com>
2018-09-06 08:33:36 +0000

    ex

--- basic/src/github.com/songjein/03.number/ex.go
+++ basic/src/github.com/songjein/03.number/ex.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"unsafe"
 )
 
 func main() {
@@ -32,8 +33,27 @@ func main() {
 	var bnum2 byte = 0x32 // 16 base
 	var bnum3 byte = 'a'  // can't use "double qoute "
 
-	// rune for unicode(UTF-8)
+	// rune for unicode(UTF-8), can't use ""
 	var r1 rune = '한'
 	var r2 rune = '\ud55c'
 	var r3 rune = '\U0000d55c'
+
+	fmt.Println(bnum, bnum2, bnum3, r1, r2, r3)
+
+	// ops for num
+	// <<, >>, ^(bit flipping)
+	// 1 + 1.2 --> error (both operand must have the same type)
+
+	// max, min value
+	var n1 uint8 = math.MaxUint8   // + 1 --> compile error!! (overflow)
+	var n2 uint16 = math.MaxUint16 // -1 --> compile error (underflow --> overflow)
+	var n3 uint32 = math.MaxUint32
+	var n4 uint64 = math.MaxUint64
+
+	fmt.Println(n1)
+	fmt.Println(n2)
+	fmt.Println(n3)
+	fmt.Println(n4)
+
+	fmt.Println(unsafe.Sizeof(n1), unsafe.Sizeof(n2)) // byte size
 }
