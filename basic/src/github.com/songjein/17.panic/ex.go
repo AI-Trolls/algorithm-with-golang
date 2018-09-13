e0eecb10a25c8f3d2151e7eb02a4671db7631c8a
songjein <jeinsong100@gmail.com>
2018-09-13 04:21:54 +0000

    panic ex

---
+++ basic/src/github.com/songjein/17.panic/ex.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+/**
+ *	panic ; error -> program termination
+ *	ex) index out of range
+ *	user can invoke the error using panic() function
+ */
+func f() {
+	defer func() {
+		s := recover()
+		fmt.Println(s)
+	}()
+
+	panic("Error !")
+	fmt.Println("vim-go")
+}
+
+func f2() {
+	defer func() {
+		s := recover()
+		fmt.Println(s)
+	}()
+
+	a := [...]int{1, 2}
+
+	for i := 0; i < 5; i++ {
+		fmt.Println(a[i])
+	}
+}
+
+func main() {
+	f()
+	f2()
+
+	fmt.Println("Hello, world")
+}
