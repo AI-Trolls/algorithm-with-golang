61474a32ed111940be87da0a8b004b9a8bd4ef0b
songjein <jeinsong100@gmail.com>
2018-09-13 02:06:05 +0000

    defer

---
+++ basic/src/github.com/songjein/16.defer/ex.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	// defer -> LIFO
+	defer world()
+
+	hello()
+	hello()
+	hello()
+
+	// defer -> LIFO ; useful case : closing file
+	defer func() {
+		fmt.Println("deeeefeeeeeeeeeeeeeeeeeeer")
+	}()
+
+	ReadHello()
+
+}
+
+func hello() {
+	fmt.Println("Hello")
+}
+
+func world() {
+	fmt.Println("World")
+}
+
+func ReadHello() {
+	file, err := os.Open("hello.txt")
+	defer file.Close()
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	buf := make([]byte, 100)
+	if _, err = file.Read(buf); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(string(buf))
+}
