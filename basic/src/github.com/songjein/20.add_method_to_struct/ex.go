4f59d39e97aaa927fcfe9f89b4561539ecbc887f
songjein <jeinsong100@gmail.com>
2018-09-13 09:38:17 +0000

    add method to struct ex

---
+++ basic/src/github.com/songjein/20.add_method_to_struct/ex.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+type Rectangle struct {
+	width  int
+	height int
+}
+
+// define receiver variable between 'func' keyword and 'name' of the function
+func (rect *Rectangle) area() int {
+	return rect.width * rect.height
+}
+
+// but there's two way of the pass struct to the receiver variable
+
+func (rect *Rectangle) scaleA(factor int) {
+	rect.width = rect.width * factor
+	rect.height = rect.height * factor
+}
+
+func (rect Rectangle) scaleB(factor int) {
+	// value just copied
+	rect.width = rect.width * factor
+	rect.height = rect.height * factor
+	// can't change struct outside
+}
+
+// if we don't need the receiver variable
+func (_ Rectangle) dummy() {
+	fmt.Println("dummy func")
+}
+
+func main() {
+	rect := Rectangle{10, 20}
+
+	fmt.Println(rect.area())
+
+	rect.dummy()
+}
