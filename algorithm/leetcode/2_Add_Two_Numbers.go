1f0cf36bcca3d65a96c131ee5fbbd0bc9a327c2f
Chanran Kim <bassilver@naver.com>
2018-09-20 10:42:29 +0900

    Update 2_Add_Two_Numbers.go

--- algorithm/leetcode/2_Add_Two_Numbers.go
+++ algorithm/leetcode/2_Add_Two_Numbers.go
@@ -17,6 +17,76 @@
  *     Next *ListNode
  * }
  */
+
+// 20 ms
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-    
+    _sumVal := 0
+    _round := 0
+    _start := l1
+    _start_l2 := l2
+    _len_l1 := 0
+    _len_l2 := 0
+    for l1 != nil || l2 != nil {
+        if(l1 == nil) {
+            _sumVal = l2.Val + _round
+            if _sumVal >= 10 {
+                _sumVal = _sumVal - 10
+                _round = 1
+            } else {
+                _round = 0
+            }
+            l2.Val = _sumVal
+            if l2.Next == nil && _round == 1 {
+                var _last *ListNode = new(ListNode)
+                _last.Val = 1
+                _round = 0
+                l2.Next = _last
+            }
+            l2 = l2.Next
+            _len_l2 =+ 1
+            continue;
+        }
+        if(l2 == nil) {
+            _sumVal = l1.Val + _round
+            if _sumVal >= 10 {
+                _sumVal = _sumVal - 10
+                _round = 1
+            } else {
+                _round = 0
+            }
+            l1.Val = _sumVal
+            if l1.Next == nil && _round == 1 {
+                var _last *ListNode = new(ListNode)
+                _last.Val = 1
+                _round = 0
+                l1.Next = _last
+            }
+            l1 = l1.Next
+            _len_l1 =+ 1
+            continue;
+        }
+        
+        _sumVal = l1.Val + l2.Val + _round
+        _round = 0
+        if _sumVal >= 10 {
+            _round = 1
+            l1.Val = _sumVal - 10
+            l2.Val = _sumVal - 10
+        } else {
+            l1.Val = _sumVal
+            l2.Val = _sumVal
+        }
+         if l1.Next == nil && l2.Next == nil && _round == 1 {
+             var _last *ListNode = new(ListNode)
+             _last.Val = 1
+             l1.Next = _last
+             return _start
+        }
+        l1 = l1.Next
+        l2 = l2.Next
+    }
+    if _len_l2 > _len_l1 {
+        return _start_l2
+    }
+    return _start
 }
