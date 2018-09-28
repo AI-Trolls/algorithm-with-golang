4d5423dc355c4961c60a87a359a52e435acf622b
Chanran Kim <bassilver@naver.com>
2018-09-28 10:31:54 +0900

    Update 2_Add_Two_Numbers.go

--- algorithm/leetcode/2_Add_Two_Numbers.go
+++ algorithm/leetcode/2_Add_Two_Numbers.go
@@ -1,5 +1,5 @@
 // LeetCode Medium
-// 2 Add Two Numbers
+// 2. Add Two Numbers
 // You are given two non-empty linked lists representing two non-negative integers.
 // The digits are stored in reverse order and each of their nodes contain a single digit.
 // Add the two numbers and return it as a linked list.
