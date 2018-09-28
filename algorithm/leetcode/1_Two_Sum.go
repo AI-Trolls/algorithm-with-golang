710c574ff3ed9cda2f3c3934c9d28da35d7e97d3
Chanran Kim <bassilver@naver.com>
2018-09-28 10:31:46 +0900

    Update 1_Two_Sum.go

--- algorithm/leetcode/1_Two_Sum.go
+++ algorithm/leetcode/1_Two_Sum.go
@@ -1,5 +1,5 @@
 // LeetCode Easy
-// 1 Two Sum in Go Language
+// 1. Two Sum in Go Language
 // Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 // You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
